adfb20db760dd336b64eab83b61d78b477530687
u-simon <u_simon@163.com>
2020-05-04 09:57:58 +0800

    poland calculator

--- src/main/java/com/simon/datastructures/stack/PolandNotation.java
+++ src/main/java/com/simon/datastructures/stack/PolandNotation.java
@@ -12,11 +12,67 @@ import java.util.Stack;
 public class PolandNotation {
 
 	public static void main(String[] args) {
-		String expression = "30 4 + 5 * 6 -";
-		System.out.println(cal(Arrays.asList(expression.split(" "))));
+		List<String> strings = toInFixExpression(
+				Arrays.asList("1", "+", "(", "(", "2", "+", "3", ")", "*", "4", ")", "-", "5"));
+
+		System.out.println(cal(strings));
 	}
 
+	/**
+	 * 中缀表达式转后缀表达式
+	 */
 
+	public static List<String> toInFixExpression(List<String> expression) {
+
+		Stack<String> stack = new Stack<>();
+		List<String> result = new ArrayList<>(100);
+		for (String exp : expression) {
+			// 数字 直接放到结果
+			if (isNum(exp.charAt(0))) {
+				result.add(exp);
+				continue;
+			}
+			// 括号
+			if ("(".equals(exp)) {
+				// 左括号
+				stack.push(exp);
+				continue;
+			}
+			if (")".equals(exp)) {
+				// 右括号
+
+				while (!"(".equals(stack.peek())) {
+					result.add(stack.pop());
+				}
+				stack.pop();
+				continue;
+			}
+			// 运算符 操作符栈为空 直接入栈
+			if (stack.empty() || "(".equals(stack.peek())) {
+				stack.push(exp);
+				continue;
+			}
+			// 和栈顶元素比较优先级
+			if (Operational.parseValue(exp).getCode() > Operational.parseValue(stack.peek())
+					.getCode()) {
+				stack.push(exp);
+				continue;
+			}
+			result.add(stack.pop());
+			stack.push(exp);
+		}
+		while (!stack.empty()){
+			result.add(stack.pop());
+		}
+		return result;
+	}
+
+	/**
+	 * 后缀表达式(逆波兰表达式)
+	 *
+	 * @param expression
+	 * @return
+	 */
 	public static String cal(List<String> expression) {
 		Stack<String> stack = new Stack<>();
 		for (String str : expression) {
